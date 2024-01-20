b93e25fd16ce8fcdd7c43053cc92c98e1dafa508
Artem Dzhemesiuk <programming@zziger.me>
2024-01-20 16:11:09 +0100

    chore: improved setup pipeline

---
+++ setup.sh
@@ -0,0 +1,4 @@
+wget https://github.com/nutti/fake-bpy-module/releases/download/20231118/fake_bpy_modules_3.6-20231118.zip
+unzip fake_bpy_modules_3.6-20231118.zip
+mv fake_bpy_modules_3.6-20231118 .vscode/fake_bpy_modules
+rm fake_bpy_modules_3.6-20231118.zip
