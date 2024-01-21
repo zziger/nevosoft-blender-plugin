09705ef1260f2e17aa6922ad08898b299e1765b2
Artem Dzhemesiuk <programming@zziger.me>
2024-01-21 14:31:10 +0100

    feat: split model tools into collapsible groups

--- src/panels/ModelToolsPanel.py
+++ src/panels/ModelToolsPanel.py
@@ -1,8 +1,9 @@
 import bpy
 
-class ModelTools(bpy.types.Panel):
-    bl_idname = 'OBJECT_PT_nevosoft_tools'
-    bl_label = 'Tools'
+class ModelToolsSkeleton(bpy.types.Panel):
+    bl_idname = 'OBJECT_PT_nevosoft_tools_skeleton'
+    bl_label = 'Skeleton tools'
+    bl_order = 0
     bl_context = ''
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'TOOLS' if bpy.app.version < (2, 80, 0) else 'UI'
@@ -11,10 +12,20 @@ class ModelTools(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         
-        layout.label(text="Replace model rest pose:")
-        layout.operator("nevosoft.retarget_animations")
-        layout.operator("nevosoft.retarget_model")
-
-        layout.label(text="Bone utils:")
         layout.operator("nevosoft.fix_bone_ids")
         layout.operator("nevosoft.place_skeleton_on_ground")
+
+class ModelToolsRetarget(bpy.types.Panel):
+    bl_idname = 'OBJECT_PT_nevosoft_tools_retarget'
+    bl_label = 'Retarget skeleton rest pose'
+    bl_order = 1
+    bl_context = ''
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS' if bpy.app.version < (2, 80, 0) else 'UI'
+    bl_category = 'Nevosoft'
+
+    def draw(self, context):
+        layout = self.layout
+        
+        layout.operator("nevosoft.retarget_animations")
+        layout.operator("nevosoft.retarget_model")
